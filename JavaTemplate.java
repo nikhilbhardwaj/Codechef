2dc123127a17b8b8b8dfe845c1ea494a051bc5a0
nikhil bhardwaj <root@nikhilbhardwaj.in>
2012-06-03 21:34:46 +0530

    Added the template as a starting point to avoid writing boilerplate

---
+++ JavaTemplate.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+class Main
+{
+  public static void main(String [] args) throws IOException
+  {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer strtok = null;
+    int numTests = Integer.parseInt( br.readLine() );
+    while(numTests-- > 0)
+    {
+      //strtok = new StringTokenizer(br.readLine());
+
+      //System.out.println();
+    }
+  }
+}
